f88be27efca1baee53966af8623c24f385191306
Augustocfq <augustocfq@gmail.com>
2025-11-18 23:46:26 -0300

    doc: documentate the contador_4bits.asm in a README.md file

--- 04-contatdor-4bits/contador_4bits.asm
+++ 04-contatdor-4bits/contador_4bits.asm
@@ -1,45 +1,38 @@
-;
-; ContadorBinario.asm
-;
-; Created: 11/5/2025 10:24:07 PM
-; Author : pedro
-; Código 3
-
-;O objetivo desse código é contar de 0 até 15 em binário usando LEDs associados a porta D.
+.nolist
+.include "m328Pdef.inc"
+.list
 
 INICIO:
-	
-	LDI R16, 0xFF
+	LDI R16, 0b11111111
 	OUT DDRD, R16
-
-	LDI R16, 0b00000000 ; R16 será o registrador responsável por contar e registrar o número que deve sair.
-;Assim, a ideia é começar com 0 e ir incrementando de um em um até 15.
+	LDI R16, 0b00000000 
 
 PRINCIPAL:
-
 	OUT PORTD, R16
-	INC R16 ; O incremento mencionado à cima.
-	RCALL ATRASO
-
+	CALL ATRASO
+	CALL INCREMENTAR
 	RJMP PRINCIPAL
-;Note que, após ultrapassar 15 (00001111) as LEDs apagam. Isso ocorre, pois 0b00001111 + 0b1 = 0b00010000. Dessa forma, os LEDs ligados a PD 0-3
-;são zerados, mas o registrador continua contando até atingir o valor 0b11111111. Para verificar isso, adicione mais LEDs no restante dos pinos da porta D.
-;OBS: 0b11111111 + 0b1 = 0b00000000 (Overflow) e 0b00000000 -0b1 = 0b11111111 (Underflow).
 
-;Exercício: No simulide, conecte outros 4 LEDs em pinos da porta B e os faça contar de 15 até 0 alterando esse código.(OBS: os LEDs da porta D devem
-; continuar a contagem normalmente). Dica: Lembre-se que os registradores r17 até r19 estão sendo utilizados na rotina de atraso. 
+INCREMENTAR:
+	CPI R16, 15
+	BREQ ZERAR
+		INC R16
+		RET
+	ZERAR:
+		LDI R16, 0
+		RET
+
 ATRASO:
-
-	LDI R17, 78
-L1:	LDI R18, 255
-L2:	LDI R19, 255
-L3:	DEC R19
-	BRNE L3
-
-	DEC R18
-	BRNE L2
-
-	DEC R17
-	BRNE L1
-
-	RET
+    LDI R17, 82 
+    L17:
+        LDI R18, 255
+    L18:
+        LDI R19, 255
+    L19:
+        DEC R19
+        BRNE L19
+        DEC R18
+        BRNE L18
+        DEC R17
+        BRNE L17
+    RET 
